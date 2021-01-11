fe1742feb6cbc57b54ac7e7cd69075fdfc23d984
Tom Youngs <tomfyoungs@gmail.com>
2021-01-11 15:48:40 +0000

    upload and display image

--- Elanco_Project/javascript/Index.js
+++ Elanco_Project/javascript/Index.js
@@ -5,13 +5,30 @@ const customTxt = document.getElementById("custom-text");
 customBtn.addEventListener("click", function() {
   realFileBtn.click();
 });
-
+  
 realFileBtn.addEventListener("change", function() {
-  if (realFileBtn.value) {
-    customTxt.innerHTML = realFileBtn.value.match(
-      /[\/\\]([\w\d\s\.\-\(\)]+)$/
-    )[1];
+    if (realFileBtn.value) {
+        var uploadFileName = realFileBtn.value.match(
+        /[\/\\]([\w\d\s\.\-\(\)]+)$/
+        )[1];
+        customTxt.innerHTML = uploadFileName;
+        const reader = new FileReader();
+
+        reader.addEventListener("load", () =>{
+            localStorage.setItem("recent-image", reader.result);
+        });
+        reader.readAsDataURL(this.files[0]);
+
+        storeImage();
   } else {
     customTxt.innerHTML = "No file chosen, yet.";
   }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const recentImageDataUrl = localStorage.getItem("recent-image");
+
+    if (recentImageDataUrl){
+        document.querySelector("#imgPreview").setAttribute("src", recentImageDataUrl);
+    }
 });
