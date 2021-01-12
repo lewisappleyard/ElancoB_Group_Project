0bd065739dd411d1aff34ae811c2559bf26e60e8
Tom Youngs <tomfyoungs@gmail.com>
2021-01-12 15:34:53 +0000

    trying to store dataUrl into a normal Url

--- Elanco_Project/javascript/main.js
+++ Elanco_Project/javascript/main.js
@@ -1,17 +1,20 @@
 const { FormRecognizerClient, FormTrainingClient, AzureKeyCredential } = require("@azure/ai-form-recognizer");
 const fs = require("fs");
+const path = require("path");
 
 const apiKey = '4a71bfacb4814c35a28c7ba817620f50';
 const endpoint = 'https://elancoproject.cognitiveservices.azure.com/';
 
 
-const recentImageDataUrl = localStorage.getItem("recent-image");
+//const recentImageDataUrl = localStorage.getItem("recent-image");
 const trainingClient = new FormTrainingClient(endpoint, new AzureKeyCredential(apiKey));
 const client = new FormRecognizerClient(endpoint, new AzureKeyCredential(apiKey));
 
 async function recognizeContent() {
-    const formUrl = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/simple-invoice.png";
-    const poller = await client.beginRecognizeContentFromUrl(recentImageDataUrl);
+    //path.join(__dirname, "./assets/contoso-allinone.jpg");
+    //const formUrl = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/simple-invoice.png";
+    const formUrl = "C:\Users\tomfy\Desktop\ElancoB_Group_Project\Elanco_Project\assets\MountPleasantReceipt.png"
+    const poller = await client.beginRecognizeContentFromUrl(formUrl);
     const pages = await poller.pollUntilDone();
 
     if (!pages || pages.length === 0) {
