848c3ec2c0c0152db506db98c4e0607129a2cb46
Tom Youngs <tomfyoungs@gmail.com>
2021-01-12 13:39:43 +0000

    improvements to the upload space

--- Elanco_Project/javascript/Index.js
+++ Elanco_Project/javascript/Index.js
@@ -1,44 +1,23 @@
-const realFileBtn = document.getElementById("real-file");
-const customBtn = document.getElementById("custom-button");
-const customTxt = document.getElementById("custom-text");
+const inpFile = document.getElementById("inpFile")
+const previewContainer = document.getElementById("imagePreview")
+const previewImage = previewContainer.querySelector(".image-preview__image")
+const previewDefaultText = previewContainer.querySelector(".image-preview__default-text")
 
-customBtn.addEventListener("click", function() {
-  realFileBtn.click();
-});
-  
-realFileBtn.addEventListener("change", function() {
-    if (realFileBtn.value) {
-        var uploadFileName = realFileBtn.value.match(
-        /[\/\\]([\w\d\s\.\-\(\)]+)$/
-        )[1];
-        customTxt.innerHTML = uploadFileName;
-        
-        const reader = new FileReader();
+inpFile.addEventListener("change", function(){
+  const file = this.files[0];
 
-        reader.addEventListener("load", () =>{
-            localStorage.setItem("recent-image", reader.result);
-        });
-        reader.readAsDataURL(this.files[0]);
-        storeImage();
-        
-  } else {
-    customTxt.innerHTML = "No file chosen, yet.";
+  console.log(file);
+
+  if(file){
+    const reader = new FileReader();
+
+    previewDefaultText.style.display = "none";
+    previewImage.style.display = "block";
+
+    reader.addEventListener("load", function(){
+        previewImage.setAttribute("src", this.result);
+    });
+
+    reader.readAsDataURL(file);
   }
 });
-
-document.addEventListener("DOMContentLoaded", () => {
-    const recentImageDataUrl = localStorage.getItem("recent-image");
-
-    if (recentImageDataUrl){
-        document.querySelector("#imgPreview").setAttribute("src", recentImageDataUrl);
-        
-        var img = new Image();
-        img = recentImageDataUrl;
-        img.onclick = function() {
-          window.location.href = 'http://127.0.0.1:5500/Elanco_Project/assets/';
-        };
-        
-    }
-});
-
-
